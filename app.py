10d60461e8a28aef18d8cc11e26ebd3f64b17139
unknown <gagarinjoshua@gmail.com>
2024-05-30 00:36:41 +0800

    working both left and right

--- app.py
+++ app.py
@@ -1,26 +1,21 @@
 from flask import Flask, render_template, request, jsonify
 
 chat_list = []
-
+response_list = []
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html',todos=chat_list)
+    return render_template('index.html',todos=chat_list, responses=response_list, zip=zip)
 
-# @app.route('/run-python-function', methods=['POST'])
-# def run_python_function():
-#     data = request.json
-#     input_value = data.get('input_value', '')
-#     result = my_python_function(input_value)
-#     return jsonify(result=result)
 
 @app.route('/add', methods=['POST'])
 def add_todo():
     task = request.form.get('task')
     if task:
         chat_list.append(task)
-    print(chat_list)
+        response_list.append(task)
+        print(chat_list)
     return jsonify({'status': 'success'})
 
 if __name__ == '__main__':
