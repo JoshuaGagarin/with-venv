7a6b61eb0b25e699837952361bdd37de4e3a7484
unknown <gagarinjoshua@gmail.com>
2024-08-08 09:08:10 +0800

    added html

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 import numpy as np
 
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Hello World!"
+# def index():
+#     return "Hello World!"
+
+def home():
+    return render_template('index.html',todos=chat_list, responses=response_list, zip=zip)
