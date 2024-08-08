bdd5dc70ff27fa6bb1a3631691d1dee5fb52c4ae
unknown <gagarinjoshua@gmail.com>
2024-08-08 09:12:53 +0800

    added static folder

---
+++ static/index.js
@@ -0,0 +1,75 @@
+const menuToggle=document.querySelector('.menuToggle');
+
+    menuToggle.onclick = function() 
+    {
+        scrollToBottom()
+        menuToggle.classList.toggle('active')
+    }
+
+    if (localStorage.getItem('isActive') === 'true') {
+        menuToggle.classList.toggle('active');
+        const input = document.getElementById('inputValue');
+            input.focus();
+      
+    }
+    
+const closeContainer = document.querySelector('.closeContainer');
+    closeContainer.onclick = function() {
+        menuToggle.classList.remove('active')
+    }
+
+    function scrollToBottom() {
+        const container = document.getElementById('secondary-div');
+        container.scrollTop = container.scrollHeight;
+    }
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+        scrollToBottom();
+        menuToggle.classList.toggle('active')
+        const form = document.getElementById('myForm');
+        const input = document.getElementById('inputValue');
+    
+        form.addEventListener('submit', function(event) {
+            event.preventDefault(); // Prevent actual form submission
+            // Perform form submission logic here
+            console.log("Hello workd")
+            // Refocus on the input field
+            input.value = ''; // Clear the input field if needed
+        });
+        input.focus();
+});
+
+
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent default form submission
+
+    var taskInput = document.getElementById('inputValue').value;
+    if (taskInput.trim() === '') return; // Don't add empty tasks
+
+    fetch('/add', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'task=' + encodeURIComponent(taskInput)
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === 'success') {
+            var taskList = document.getElementById('taskList');
+            var newTaskItem = document.createElement('p');
+            newTaskItem.classList.add('task');
+            newTaskItem.textContent = taskInput;
+            taskList.appendChild(newTaskItem);
+            // document.getElementById('taskInput').value = ''; // Clear input field
+            location.reload();
+            // scrollToBottom();
+
+            // localStorage.setItem('isActive', 'true');
+        }
+    })
+    .catch(error => console.error('Error:', error));
+});
+
+
